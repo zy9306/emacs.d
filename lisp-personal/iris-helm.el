7a365beb699c4d8eaf091236f22e5c2ffbb7d46e
zhengyu <yuzheng@gongpingjia.com>
2018-06-26 23:06:11 +0800

    add some helm key binding

--- lisp-personal/iris-helm.el
+++ lisp-personal/iris-helm.el
@@ -16,6 +16,9 @@
   (global-set-key (kbd "C-h r") 'helm-info-emacs)
   (global-set-key (kbd "C-h C-l") 'helm-locate-library)
   (global-set-key (kbd "C-c f") 'helm-recentf)
+  (global-set-key (kbd "C-c h d a") 'helm-do-ag)
+  (global-set-key (kbd "C-c h s i") 'helm-semantic-or-imenu)
+  (global-set-key (kbd "C-c h i") 'helm-imenu)
 
   (substitute-key-definition 'find-tag 'helm-etags-select global-map)
   (define-key minibuffer-local-map (kbd "C-c C-l") 'helm-minibuffer-history)
