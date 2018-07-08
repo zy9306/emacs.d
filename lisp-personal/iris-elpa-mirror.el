6dda6a8ba5df252a2ff2eccba8f1b17800c76bb4
zhengyu <yuzheng@gongpingjia.com>
2018-07-08 15:05:16 +0800

    elpa-mirror

---
+++ lisp-personal/iris-elpa-mirror.el
@@ -0,0 +1,9 @@
+;; https://github.com/redguardtoo/elpa-mirror
+;; 建立本地备份
+;; M-x elpamr-create-mirror-for-installed to create local repository.
+;; M-x elpamr-create-mirror-for-installed command again for update
+(require 'elpa-mirror)
+(setq elpamr-default-output-directory "~/Nutstore/myelpa")
+
+
+(provide 'iris-elpa-mirror)
