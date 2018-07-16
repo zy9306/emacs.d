fb83df498031816d73879016a5f1c28c2d9dd56f
zhengyu <yuzheng@gongpingjia.com>
2018-07-16 21:52:02 +0800

    highlight

--- custom.el
+++ custom.el
@@ -142,6 +142,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight ((t (:background "#E3E3F2" :foreground "#655370"))))
+ '(region ((t (:background "light coral"))))
  '(imenu-list-entry-face-0 ((t (:inherit imenu-list-entry-face :foreground "firebrick"))))
  '(imenu-list-entry-face-1 ((t (:inherit imenu-list-entry-face :foreground "slate blue"))))
  '(imenu-list-entry-face-2 ((t (:inherit imenu-list-entry-face :foreground "dark cyan"))))
