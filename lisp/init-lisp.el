0a509043e932812c6444003068e7fe5d7798257a
zy <zy@debian>
2018-09-20 21:33:07 +0800

    setq-default scratch message

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -3,8 +3,8 @@
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
 (add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "ELisp")))
 
-(setq-default initial-scratch-message
-              (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
+;; (setq-default initial-scratch-message
+;;               (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
 
 
 
