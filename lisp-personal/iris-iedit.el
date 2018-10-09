86e434805580f5f4e4b52775e16cf5d5dcbe2ff7
zhengyu <yuzheng@gongpingjia.com>
2018-10-09 13:47:50 +0800

    iedit

--- lisp-personal/iris-iedit.el
+++ lisp-personal/iris-iedit.el
@@ -2,9 +2,7 @@
   ;; https://github.com/victorhge/iedit/blob/master/iedit.el
   :ensure t
   :config
-  ;; 原avy-goto-char-timer,不解绑每次启动会有警告
-  (global-unset-key (kbd "C-;"))
-  ;; (global-set-key (kbd "C-c ;") 'iedit-mode-toggle-on-function) todo
+  ;; todo keybinding
   )
 
 (provide 'iris-iedit)
